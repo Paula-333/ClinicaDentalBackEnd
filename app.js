ee92c4128b8e03a986cb22313cdc695df14407f8
Paula-333 <71704680+Paula-333@users.noreply.github.com>
2020-11-09 13:56:16 +0100

    Tablas migradas

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+//importamos librerias
+const mysql = require('mysql2/promise');
+//const json  = require('sequelize/types');
+const userRouter = require('./routers/userRouter.js')
+const citaRouter = require('./routers/citaRouter.js')
+const historialRouter = require('./routers/historialRouter.js')
+
+
+const app = express();
+const PORT = 3330;
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'clinica_dental_citas',
+    password:'123456789'
+})
+.then(() => console.log('Sequelize conectado'))
+.catch((error) => console.log('Error en conectar Sequelize', error));
+
+
+app.use(express.json());
+
+
+//CORS//
+app.use(function(req, res, next) { //para evitar el error CORS
+    res.header("Access-Control-Allow-Origin", "*"); //permite hacer peticiones desde todos los orígenes
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); //permite peticiones con las cabeceras enumeradas
+    // res.header("Access-Control-Allow-Methods", "GET, POST");
+    next();
+});
+
+/*app.get('/', async(req, res)=>{
+    try{
+        const db = await conn;
+        const [citas] = await db.execute('SELECT * FROM citas');
+        console.log(citas);
+        res.json(citas);
+    }catch (error){
+        console.error(error);
+        res.status(500).send({
+            message:'Error en citas'
+        });
+    }
+
+});*/
+
+
+
+//Enrutado a endpoints 
+
+app.use('/user/', userRouter);
+app.use('/cita/', citaRouter);
+app.use('/historial/', historialRouter);
+
+
+
+
+app.listen(PORT, () => console.log(`Servidor funcionando en puerto ${PORT}`));
