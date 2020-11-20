c86a75b67250cfe15ad12ebdf95cba52ce33e4da
Paula-333 <71704680+Paula-333@users.noreply.github.com>
2020-11-20 18:12:22 +0100

    Probando sacar todas las citas

--- controller/citaController.js
+++ controller/citaController.js
@@ -29,6 +29,18 @@ module.exports.createCita = async (req, res) => {
         })
     }
 };          
+////...::TODAS LAS CITAS::...////
+
+module.exports.showAll = async (req, res) => {
+    Cita.findAll()
+    .then(citas => res.send(citas))
+    .catch(error => {
+        console.error(error);
+        res.status(500).send({
+            message: 'Ha habido un problema tratando de recuperar las citas'
+        })
+    })
+}
 
 ////...::BUSCAR CITA PENDIENTE::...////
 
--- route/citaRouter.js
+++ route/citaRouter.js
@@ -3,6 +3,7 @@ const CitaController = require('../controller/citaController.js');
 
 //Ruta
 router.post('/createCita', CitaController.createCita);//CREAR CITA
+router.get('/showAll', CitaController.showAll)//TODAS LAS CITAS
 router.get('/findCita/:id', CitaController.findCita);//CITAS PENDIENTES
 router.delete('/remove', CitaController.remove);//BORRAR CITA
 
